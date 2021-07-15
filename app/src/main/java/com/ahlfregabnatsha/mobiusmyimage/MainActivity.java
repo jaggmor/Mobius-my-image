54bcad3f7a8799caf5ac909b6d04f760d1b3a16c
gabriel-shafiqahlgren <gabriel.shaahl@gmail.com>
2021-07-16 01:27:36 +0200

    Add permission requests for camera and reading writing files

--- app/src/main/java/com/ahlfregabnatsha/mobiusmyimage/MainActivity.java
+++ app/src/main/java/com/ahlfregabnatsha/mobiusmyimage/MainActivity.java
@@ -1,14 +1,97 @@
 package com.ahlfregabnatsha.mobiusmyimage;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Defining my codes for camera
+    private static final int MY_CAMERA_PERMISSION_CODE = 1;
+    private static final int MY_FILE_PERMISSION_CODE = 2;
+    private static final int REQUEST_IMAGE_CAPTURE = 3;
+
+    // Declaring ImageButtons (!=Button)
+    private ImageButton camera, folder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        camera = findViewById(R.id.btn_camera);
+        folder = findViewById(R.id.btn_folder);
+
+        camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED) {
+
+                    // Requesting the permission
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[] { Manifest.permission.CAMERA }, MY_CAMERA_PERMISSION_CODE);
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "Permission already granted", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        folder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+
+                    // Requesting the permission
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[] { Manifest.permission.WRITE_EXTERNAL_STORAGE }, MY_FILE_PERMISSION_CODE);
+                }
+                else {
+                    Toast.makeText(MainActivity.this,
+                            "Permission already granted", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    // This function is called when the user accepts or decline the permission.
+    // Request Code is used to check which permission called this function.
+    // This request code is provided when the user is prompt for permission.
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults)
+    {
+        super.onRequestPermissionsResult(requestCode,
+                permissions,
+                grantResults);
+
+        if (requestCode == MY_CAMERA_PERMISSION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(MainActivity.this, "Camera Permission Granted", Toast.LENGTH_SHORT) .show();
+            }
+            else {
+                Toast.makeText(MainActivity.this, "Camera Permission Denied", Toast.LENGTH_SHORT) .show();
+            }
+        }
+        else if (requestCode == MY_FILE_PERMISSION_CODE) {
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(MainActivity.this, "Storage Permission Granted", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(MainActivity.this, "Storage Permission Denied", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
