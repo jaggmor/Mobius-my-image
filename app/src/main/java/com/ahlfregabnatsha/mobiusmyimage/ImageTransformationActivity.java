8ef1426d0cb43aa350a8c58b882a52783ad88b92
gabriel-shafiqahlgren <gabriel.shaahl@gmail.com>
2021-07-29 00:28:46 +0200

    Implemented onTouchListener and removed the Transform-button. Proof of concept is working!

--- app/src/main/java/com/ahlfregabnatsha/mobiusmyimage/ImageTransformationActivity.java
+++ app/src/main/java/com/ahlfregabnatsha/mobiusmyimage/ImageTransformationActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.annotation.SuppressLint;
 import android.app.Dialog;
 import android.content.Intent;
 import android.content.res.Resources;
@@ -16,20 +17,38 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.IOException;
 
 public class ImageTransformationActivity extends AppCompatActivity {
 
     private ImageView imageView;
-    private Button btn_transform;
+    //private Button btn_transform;
     public Bitmap bitmap;
+    public int bitmapWidth;
+    public int bitmapHeight;
+
+    private ComplexNumber z1;
+    private ComplexNumber w1;
+
+    private ComplexNumber z2;
+    private ComplexNumber w2;
+
+    private ComplexNumber z3;
+    private ComplexNumber w3;
+
+    private ComplexNumber[] complexNumberPoints = new ComplexNumber[6];
+
+    int points_picked = 0;
 
     Dialog dialog;
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -59,56 +78,143 @@ public class ImageTransformationActivity extends AppCompatActivity {
             bitmap = resizeToScreenWidth(bitmap);
         }
 
+        this.bitmapWidth = bitmap.getWidth();
+        this.bitmapHeight = bitmap.getHeight();
+
+        Toast.makeText(ImageTransformationActivity.this,
+                Integer.toString(bitmapWidth), Toast.LENGTH_SHORT).show();
+        Toast.makeText(ImageTransformationActivity.this,
+                Integer.toString(bitmapHeight), Toast.LENGTH_SHORT).show();
+
+
         imageView = findViewById(R.id.photoImageView);
         imageView.setImageBitmap(bitmap);
+        
+        
+        imageView.setOnTouchListener(new View.OnTouchListener() {
 
-        btn_transform = findViewById(R.id.btn_transform);
+            @SuppressLint("ClickableViewAccessibility")
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                int action = event.getAction();
+                if (action == MotionEvent.ACTION_DOWN && points_picked < 6){
+                    complexNumberPoints[points_picked] = new ComplexNumber(
+                            x_center((int) event.getX()), y_center((int) event.getY()));
+                    Toast.makeText(ImageTransformationActivity.this,
+                            complexNumberPoints[points_picked].toString(), Toast.LENGTH_SHORT) .show();
+                    points_picked++;
+                    return true;
+                }
+                else if (points_picked == 6){
+                    z1 = complexNumberPoints[0];
+                    w1 = complexNumberPoints[1];
+                    z2 = complexNumberPoints[2];
+                    w2 = complexNumberPoints[3];
+                    z3 = complexNumberPoints[4];
+                    w3 = complexNumberPoints[5];
+                    MobiusTransformation mt = new MobiusTransformation(z1, z2, z3, w1, w2, w3);
+
+                    bitmap = transformBitmap(bitmap, mt);
+                    imageView.setImageBitmap(bitmap);
+                    points_picked++;
+                    return false;
+                }
+                else {
+                    return false;
+                }
+
+            }
+        });
+        
+        
+
+        //btn_transform = findViewById(R.id.btn_transform);
+        
+         /*
         btn_transform.setOnClickListener(new View.OnClickListener() {
             //When user clicks TRANSFORM
+
             @Override
             public void onClick(View v) {
                 //Implement onTouch listener here.
-                ComplexNumber z1 = new ComplexNumber(-1, 0);
-                ComplexNumber w1 = new ComplexNumber(0, 2);
+                z1 = new ComplexNumber(-100, 0);
+                w1 = new ComplexNumber(0, 100);
 
-                ComplexNumber z2 = new ComplexNumber(0, 1);
-                ComplexNumber w2 = new ComplexNumber(2, 0);
+                z2 = new ComplexNumber(0, 100);
+                w2 = new ComplexNumber(120, 0);
 
-                ComplexNumber z3 = new ComplexNumber(1, 0);
-                ComplexNumber w3 = new ComplexNumber(0, -2);
+                z3 = new ComplexNumber(100, 0);
+                w3 = new ComplexNumber(0, -100);
+
 
                 //Transformation that will take us between bitmaps.
                 MobiusTransformation mt = new MobiusTransformation(z1, z2, z3, w1, w2, w3);
 
                 bitmap = transformBitmap(bitmap, mt);
                 imageView.setImageBitmap(bitmap);
+
+                //test rotation and zoom transformation
+                
+                imageView.setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View v, MotionEvent event) {
+                        int action = event.getAction();
+                        if (action == MotionEvent.ACTION_DOWN && points_picked < 6){
+                            complexNumberPoints[points_picked] = new ComplexNumber(
+                                    x_center((int) event.getX()), y_center((int) event.getY()));
+                            Toast.makeText(ImageTransformationActivity.this,
+                                    complexNumberPoints[points_picked].toString(), Toast.LENGTH_SHORT) .show();
+                            points_picked++;
+                            return true;
+                        }
+                        else if (points_picked == 6){
+                            z1 = complexNumberPoints[0];
+                            w1 = complexNumberPoints[1];
+                            z2 = complexNumberPoints[2];
+                            w2 = complexNumberPoints[3];
+                            z3 = complexNumberPoints[4];
+                            w3 = complexNumberPoints[5];
+                            MobiusTransformation mt = new MobiusTransformation(z1, z2, z3, w1, w2, w3);
+
+                            bitmap = transformBitmap(bitmap, mt);
+                            imageView.setImageBitmap(bitmap);
+                            return true;
+                        }
+                        else {
+                            return false;
+                        }
+
+                    }
+                });
+
             }
         });
+        */
     }
 
     //Returns the transformed bitmap.
     private Bitmap transformBitmap(Bitmap bitmap, MobiusTransformation mt) {
-        int width = bitmap.getWidth();
-        int height = bitmap.getHeight();
-        Bitmap transformedBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        Bitmap transformedBitmap = Bitmap.createBitmap(bitmapWidth, bitmapHeight, Bitmap.Config.ARGB_8888);
 
         ComplexNumber imageCoordinates, domainCoordinates;
-        for (int x = 0; x < width; x++) {
-            for (int y = 0; y < height; y++) {
-                //Adjust to coordinates with origo in center of picture.
-                imageCoordinates = new ComplexNumber(x - width/2, height/2 - y);
+        for (int x = 0; x < bitmapWidth; x++) {
+            for (int y = 0; y < bitmapHeight; y++) {
+                //Adjust to coordinates with origin in center of picture.
+                //imageCoordinates = new ComplexNumber(x - bitmapWidth / 2, bitmapHeight / 2 - y);
+                imageCoordinates = new ComplexNumber(x_center(x), y_center(y));
                 domainCoordinates = mt.transformInverse(imageCoordinates);
-                if (domainCoordinates.getReal()<width/2 && domainCoordinates.getReal()>-width/2 &&
-                        domainCoordinates.getImaginary()<height/2 && domainCoordinates.getImaginary()>-height/2) {
+                if (Math.round(domainCoordinates.getReal()) < bitmapWidth/2 && Math.round(domainCoordinates.getReal()) > -bitmapWidth/2 &&
+                        Math.round(domainCoordinates.getImaginary()) < bitmapHeight / 2 && Math.round(domainCoordinates.getImaginary()) > -bitmapHeight/2) {
 
                     //interpolation method, nearest neighbor.
-                    int xx = (int)Math.round(domainCoordinates.getReal()) + width/2;
-                    int yy = -(int)Math.round(domainCoordinates.getImaginary()) + height/2 ;
+                    //int xx = (int) Math.round(domainCoordinates.getReal()) + bitmapWidth / 2;
+                    //int yy = -(int) Math.round(domainCoordinates.getImaginary()) + bitmapHeight / 2;
+                    int xx = x_normal((int) Math.round(domainCoordinates.getReal()));
+                    int yy = y_normal((int) Math.round(domainCoordinates.getImaginary()));
                     int newPixel = bitmap.getPixel(xx, yy);
 
                     transformedBitmap.setPixel(x, y, newPixel);
-                }
-                else {
+                } else {
                     transformedBitmap.setPixel(x, y, Color.BLACK);
                 }
             }
@@ -117,6 +223,24 @@ public class ImageTransformationActivity extends AppCompatActivity {
         bitmap.recycle();   //Save memory
         return transformedBitmap;
     }
+
+    private int x_center(int x) {
+        return x - (bitmapWidth/2);
+    }
+
+    private int y_center(int y) {
+        return (bitmapHeight/2) - y;
+    }
+
+    private int x_normal(int x_c) {
+        return x_c + (bitmapWidth/2);
+    }
+
+    private int y_normal(int y_c) {
+        return (bitmapHeight/2) - y_c;
+    }
+
+
 
     //https://stackoverflow.com/questions/4837715/how-to-resize-a-bitmap-in-android
     //Rescale to functional size for transformation.
@@ -177,5 +301,4 @@ public class ImageTransformationActivity extends AppCompatActivity {
         });
         dialog.show();
     }
-
 }
